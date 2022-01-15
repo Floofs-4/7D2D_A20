c99c13e8b3c7bc9dad67ea436f07983935f2830e
Floofs-4 <97810862+Floofs-4@users.noreply.github.com>
2022-01-15 16:38:04 -0600

    Harmony/

---
+++ ZombieZen/ZombieZen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Audio;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Harmony
+{
+    public class ZombieZen : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            Log.Out(" Loading Patch: " + GetType());
+
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(EntityZombie))]
+    [HarmonyPatch("StartRage")]
+    public class NoZombieRage
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
